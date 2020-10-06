fb4dea91ff48ee57bef08c671a9b8341a41a22e9
Petr <60181770+xdanca01@users.noreply.github.com>
2020-10-06 13:33:53 +0200

    P prod (#1)
    * added basic roulette
    
    * added roulette
    
    Co-authored-by: xdanca01 <xdanca01@stud.fit.vutbr.cz>

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import sys
 import user
 import automat
 import time
+import ruleta
 
 def main():
     users = []
-    us = user.user("novak")
+    us = user.User("novak")
+    users.append(user)
     while True:
         print(us.get_name(), us.get_balance())
-        numbs = automat.automat(us, 1)
-        print(numbs)
+        #numbs = automat.automat(us, 1)
+        #print(numbs)
         time.sleep(0.5)
+        numbs = ruleta.ruleta(1,0,us)
+        print("ruleta: ",numbs)
+        print(us.get_name(), us.get_balance())
 
 
 exit(main())
