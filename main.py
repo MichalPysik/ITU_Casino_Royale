dd3eaa625d4c658e96fb958594e97bd092053fdd
xdanca01 <xdanca01@stud.fit.vutbr.cz>
2020-10-11 19:13:48 +0200

    api for saving and loading users

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import user
 import automat
 import time
 import ruleta
+import pickle
 
 
 def main():
-    users = []
-    us = user.User("novak")
+
+    # load saved users
+    users = user.get_users()
+    # list is empty
+    if not users:
+        users.append(user.User('Pepa'))
+    #us = user.User("novak")
+    us = users[0]
+    print("balance",us.get_balance())
     users.append(us)
     while True:
         print(us.get_name(), us.get_balance())
@@ -16,11 +24,9 @@ def main():
         numbs = ruleta.ruleta(1,"black",us)
         print("ruleta: ",numbs)
         print(us.get_name(), us.get_balance())
+        us.add_balance(10000)
+        break
         if us.get_balance() <= 0:
-            break;
-    for usr in users:
-        print(usr.get_name())
-        usr.save()
-
+            break
 
 exit(main())
