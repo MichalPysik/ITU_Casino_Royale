596ee2cf2ef2d51c6615df11f8bece61fa323826
xdanca01 <xdanca01@stud.fit.vutbr.cz>
2020-10-19 01:04:24 +0200

    Added more items to Slot Machines window

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import user
 import automat
-import time
 import ruleta
-import pickle
 import dice
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QGridLayout, QSizePolicy, QLineEdit, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QGridLayout, QSizePolicy, QMainWindow, QMenuBar, \
+    QMenu, QSpinBox, QTableWidget, QTableView
 from PyQt5.QtGui import QFont
 
 main_win = ""
@@ -18,13 +17,16 @@ def gui_kostky():
     print("hello")
     pass
 
+
 def gui_ruleta():
     pass
+
 
 def action_automat():
     global us
     automat.automat(us,10)
     return
+
 
 def gui_automat():
 
@@ -32,27 +34,71 @@ def gui_automat():
     global widget
     global us
 
+    #new widget to replace main menu
     aut_wid = QWidget(main_win)
     aut_wid.setStyleSheet("background-color: red ")
-    layout2 = QGridLayout(aut_wid)
-    layout = QGridLayout(aut_wid)
+    aut_wid.setGeometry(0, 0, main_win.width(), main_win.height())
 
+    #main grid
+    layout2 = QGridLayout(aut_wid)
+    #grid for 3 Slots
+    layout = QGridLayout()
+    #grid for buttons and bet
+    layout3 = QGridLayout()
+
+    #set layout as main layout
     aut_wid.setLayout(layout2)
-    layout2.addLayout(layout, 2, 2)
-    aut_wid.setGeometry(500, 500, main_win.width(), main_win.height())
+
+    #set layout for slots view
+    layout2.addLayout(layout, 2, 1)
+    #set layout for control bar
+    layout2.addLayout(layout3,3,2)
+
+    #set table with prices
+    table = QTableWidget(10, 2, aut_wid)
+    table.setHorizontalHeaderLabels(["Sign", "Win"])
+    Wins = []
+    Signs = []
+    table.setFixedSize(204,360)
+    table.verticalHeader().hide()
+    layout2.addWidget(table, 2, 2)
+
+
+
+
+
     Slot1 = QWidget(aut_wid)
+    #Slot1.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+    Slot1.setGeometry(0, 0, 80, 80)
+    Slot1.setStyleSheet(".QWidget { background-color: black} ")
     layout.addWidget(Slot1, 1, 1)
+
     Slot2 = QWidget(aut_wid)
-    layout.addWidget(Slot2, 2, 1)
+    Slot2.setGeometry(0,0,80,80)
+    Slot2.setStyleSheet(".QWidget { border: 2px solid black } ")
+    layout.addWidget(Slot2, 1, 2)
+
     Slot3 = QWidget(aut_wid)
-    layout.addWidget(Slot3, 3, 1)
+    Slot3.setGeometry(0,0,80,80)
+    Slot3.setStyleSheet(".QWidget { border: 2px solid black } ")
+    layout.addWidget(Slot3, 1, 3)
+
     btn = QPushButton(aut_wid)
     btn.clicked.connect(action_automat)
-    layout2.addWidget(btn, 1, 2)
+    #btn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+    btn.setGeometry(0,0,70,40)
+    layout3.addWidget(btn, 1, 1)
+
+    bet = QSpinBox(aut_wid)
+    layout3.addWidget(bet, 1, 3)
+
+    bet_text = QLabel(aut_wid)
+    bet_text.setText("bet:")
+    layout3.addWidget(bet_text, 1,2)
 
 
-    widget.hide()
-    aut_wid.show()
+    #widget.hide()
+    #aut_wid.show()
     Slot1.setStyleSheet("background-color: red")
     main_win.setCentralWidget(aut_wid)
     main_win.update()
@@ -73,6 +119,18 @@ def main():
     main_win = QMainWindow()
     main_win.setGeometry(0, 0, 1000, 1000)
 
+    menubar = QMenuBar(main_win)
+    menu = QMenu("Change user")
+    menu2 = QMenu("Menu")
+    menu3 = QMenu("Help")
+
+
+    menubar.addMenu(menu2)
+    menubar.addMenu(menu)
+    menubar.addMenu(menu3)
+
+
+    main_win.setMenuBar(menubar)
 
     widget = QWidget()
     main_win.setCentralWidget(widget)
@@ -83,7 +141,7 @@ def main():
     layout = QGridLayout(widget)
     widget.setLayout(layout)
 
-    grid_games = QGridLayout(widget)
+    grid_games = QGridLayout()
     layout.addLayout(grid_games, 2, 1)
 
     title = QLabel(widget)
@@ -120,10 +178,6 @@ def main():
     grid_games.addWidget(btn_automat, 1, 3)
 
     main_win.show()
-    widget.show()
-
-
-
 
     # load saved users
     users = user.get_users()
