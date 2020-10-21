8dff7962d22eddea420f5503fb07ff807ed6fa28
Lukáš Václavek <lv.vaclavek@gmail.com>
2020-10-21 22:29:08 +0200

    No balance = no bets

--- ruleta.py
+++ ruleta.py
@@ -11,6 +11,9 @@ def generovani():
 # @arg2 = number or string red, black, even, odd
 def ruleta(user, bet, choice):
 
+    if user.get_balance() < bet:
+        return False
+
     cervena = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]
     cerna = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35]
 
